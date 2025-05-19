cc5ccabdaa832b05900214c0b27cc79a180bc179
Ugo Vidberg <ugo.vidberg@gmail.com>
2025-05-19 15:02:09 +0200

    rails  stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
+# Rails.application.routes.draw do
+#   get 'questions/ask'
+#   get 'questions/answer'
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  root to: 'questions#ask'
   # Defines the root path route ("/")
   # root "posts#index"
 end
